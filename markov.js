42899855fb4a48b5699e089dc02878b700d86860
Nathan Rydel <nathanrydel@gmail.com>
2024-01-02 16:47:13 -0700

    implemented getChains method

--- markov.js
+++ markov.js
@@ -15,7 +15,7 @@ class MarkovMachine {
   /** Get markov chain: returns object of Markov chains.
    *
    *  For text of "The cat in the hat.", chains will be:
-   * 
+   *
    *  {
    *   "The": ["cat"],
    *   "cat": ["in"],
@@ -23,11 +23,30 @@ class MarkovMachine {
    *   "the": ["hat."],
    *   "hat.": [null],
    *  }
-   * 
+   *
    * */
 
   getChains() {
-    // TODO: implement this!
+    // make a new object
+    const chains = {};
+
+    // need to iterate over an input
+    // add current word to object as key
+    // add next word as a value in an array or null if end of input
+
+    for (let i = 0; i < this.words.length; i++) {
+      const currentWord = this.words[i];
+      const nextWord = this.words[i + 1] || null;
+
+      // check if word exists
+      if (currentWord in chains) {
+        chains[currentWord].push(nextWord);
+      } else {
+        chains[currentWord] = [nextWord];
+      }
+    }
+
+    return chains;
   }
 
 
@@ -42,3 +61,8 @@ class MarkovMachine {
     // - repeat until reaching the terminal null
   }
 }
+
+
+module.exports = {
+  MarkovMachine
+};
