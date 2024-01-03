14cc0849fa718937ef4b8a7dbb29f3d28d317d5d
Matt Smida <mcs@m4t7.com>
2024-01-02 20:42:56 -0500

    added tests for getText

--- markov.test.js
+++ markov.test.js
@@ -18,6 +18,8 @@ describe("Markov Machine Tests", function () {
 
   test("getChains test with duplicate words", function () {
     let machine = new MarkovMachine("the cat in the hat");
+    // the hat        VALID
+    // the cat the    INVALID  there's no 'cat the' bigram
 
     expect(machine.chains).toEqual({
       the: ['cat', 'hat'],
@@ -25,10 +27,26 @@ describe("Markov Machine Tests", function () {
       in: ['the'],
       hat: [null]
     });
-  })
-    ;
+  });
 
-  // test("getText test", function () {
+  test("getText test with deterministic input/output", function () {
+    let machine = new MarkovMachine("The cat is in the hat.");
+    expect(machine.getText()).toEqual("The cat is in the hat.");
+  });
 
-  // });
+
+  test("getText test with random output", function () {
+    let machine = new MarkovMachine("the cat in the hat");
+    expect(machine.getText()).not.toContain("the cat the");
+  });
+
+
+  test("getText test with empty string input", function () {
+    let machine = new MarkovMachine("");
+    expect(machine.getText()).toEqual("");
+  });
+
+
+
+
 });
